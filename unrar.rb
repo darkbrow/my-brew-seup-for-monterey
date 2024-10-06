732b4faaaa4834723c49f1a364fdc52753076448
Jinbong <darkbrow@gmail.com>
2024-10-07 07:54:50 +0900

    Initial commit

---
+++ unrar.rb
@@ -0,0 +1,43 @@
+class Unrar < Formula
+  desc "Extract, view, and test RAR archives"
+  homepage "https://www.rarlab.com/"
+  url "https://www.rarlab.com/rar/unrarsrc-7.0.9.tar.gz"
+  sha256 "505c13f9e4c54c01546f2e29b2fcc2d7fabc856a060b81e5cdfe6012a9198326"
+
+  livecheck do
+    url "https://www.rarlab.com/rar_add.htm"
+    regex(/href=.*?unrarsrc[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  def install
+    ENV.cxx11
+
+    # upstream doesn't particularly care about their unix targets,
+    # so we do the dirty work of renaming their shared objects to
+    # dylibs for them.
+    inreplace "makefile", "libunrar.so", "libunrar.dylib"
+
+    system "make"
+    bin.install "unrar"
+
+    # Explicitly clean up for the library build to avoid an issue with an
+    # apparent implicit clean which confuses the dependencies.
+    system "make", "clean"
+    system "make", "lib"
+    lib.install shared_library("libunrar")
+  end
+
+  test do
+    contentpath = "directory/file.txt"
+    rarpath = testpath/"archive.rar"
+    data =  "UmFyIRoHAM+QcwAADQAAAAAAAACaCHQggDIACQAAAAkAAAADtPej1LZwZE" \
+            "QUMBIApIEAAGRpcmVjdG9yeVxmaWxlLnR4dEhvbWVicmV3CsQ9ewBABwA="
+
+    rarpath.write data.unpack1("m")
+    assert_equal contentpath, `#{bin}/unrar lb #{rarpath}`.strip
+    assert_equal 0, $CHILD_STATUS.exitstatus
+
+    system "#{bin}/unrar", "x", rarpath, testpath
+    assert_equal "Homebrew\n", (testpath/contentpath).read
+  end
+end
